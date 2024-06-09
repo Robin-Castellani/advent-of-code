f108da3f3d277b77e1db6a08172789e5e720e5bc
Robin Castellani <r.castellani@eoliann.com>
2024-06-09 12:05:59 +0200

    Feat: code to solve 2023 day 1

--- aoc/main_2023_01.py
+++ aoc/main_2023_01.py
@@ -1,2 +1,18 @@
 def read_calibration(input_calibration: str) -> int:
-    return 142
+    integers_as_strings = tuple(map(str, [1, 2, 3, 4, 5, 6, 7, 8, 9]))
+
+    calibration_value = 0
+    for line in input_calibration.split("\n"):
+        number_as_string = ""
+        for letter in line:
+            if letter in integers_as_strings:
+                number_as_string += letter
+
+        if len(number_as_string) == 1:
+            number_as_string += number_as_string
+        if len(number_as_string) > 2:
+            number_as_string = number_as_string[0] + number_as_string[-1]
+
+        calibration_value += int(number_as_string)
+
+    return calibration_value
